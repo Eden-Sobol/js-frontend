d6dc89e6ab57fb5bf8304212d9c9c759661f1bdd
Eden <115366725+Eden-Sobol@users.noreply.github.com>
2022-10-08 13:12:01 -0400

    Delete create.js

--- create.js
+++
@@ -1,1 +0,0 @@
-function create(name, type, content, id) {name = document.createElement(type);name.innerHTML = content;name.id = id;name.style.position = "absolute";document.body.innerHTML += name.outerHTML}
