cf5ed09743c6375bf071a77e40feb1ede766a2bf
Eden <115366725+Eden-Sobol@users.noreply.github.com>
2022-10-23 17:36:27 -0400

    Update create.js

--- files/create.js
+++ files/create.js
@@ -1,1 +1,1 @@
-function create(type, content, id) {var name;name = document.createElement(type);name.innerHTML = content;name.id = id;name.style.position = "absolute";document.body.innerHTML += name.outerHTML}
+function create(type, content, id, left, top) {var name;name = document.createElement(type);name.innerHTML = content;name.id = id;name.style.position = "absolute";name.style.left=left;name.style.top=top;document.body.innerHTML += name.outerHTML}
