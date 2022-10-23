72a8d1fda7a378033ea0527dbaca6f309ff5ec2d
Eden <115366725+Eden-Sobol@users.noreply.github.com>
2022-10-23 17:40:46 -0400

    Update edit.js

--- files/edit.js
+++ files/edit.js
@@ -1,1 +1,1 @@
-let jsf={e:function(id,newContent,editType){if(editType=="inner"){document.getElementById(id).innerHTML=newContent}if(editType=="outer"){document.getElementById(id).outerHTML="<"+newContent+">"}}
+let jsf={e:function(id,newContent,editType){if(editType=="inner"){document.getElementById(id).innerHTML=newContent}if(editType=="outer"){document.getElementById(id).outerHTML="<"+newContent+">"}}}
